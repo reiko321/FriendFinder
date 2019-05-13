21961b0ddf385bebd6acb7431278a5dddfcdaa2f
reiko321 <reiko321@gmail.com>
2019-05-12 17:02:01 -0700

    pushing to Heroku

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+let friends = [
+	
+	{
+		name: "Mysterious Beauty",
+		photo: "https://4.bp.blogspot.com/-tqtNgxu05yk/WtwW7azpOFI/AAAAAAABWAE/dJvWyewhKKci-C8G8LM4QpPYdcy1VW6KwCLcBGAs/s640/tumblr_p74mn3stK91u7nll5o1_1280.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Handsome Stranger",
+		photo: "https://i.pinimg.com/474x/f1/83/ea/f183ea4edb5e82e0bf1a7de063157986--character-inspiration-eye-candy.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Little Lass",
+		photo: "http://2.bp.blogspot.com/-lf-e2Fq_zVk/VZZTaOC99FI/AAAAAAAASDo/SGP3DpYIbTM/kamille%2Bfiloteo%2B001.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
--- app/server.js
+++ app/server.js
@@ -19,3 +19,15 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("../app/routing/api-routes")(app);
+require("../app/routing/html-routes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
