df938b02684a27846ad9044505fedcd6bbc38aac
reiko321 <reiko321@gmail.com>
2019-06-13 16:52:19 -0700

    updated structure for heroku

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -3,11 +3,11 @@ let friends = require('../data/friends.js');
 
 module.exports = function (app) {
     
-    app.get('/api/friends', function (req, res) {
+    app.get('/friends', function (req, res) {
         res.json(friends);
     });
     
-    app.post('/api/friends', function (req, res) {
+    app.post('/friends', function (req, res) {
         
         let bestMatch = {
             name: "",
